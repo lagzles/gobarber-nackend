1e3263732e01f15a7338508b4dc5f4881cd86817
Leandro Arns <leandro.arns@gmail.com>
2020-05-30 14:10:38 -0300

    criando e aplicando o conceito de controllers

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,36 +1,18 @@
 import { Router } from 'express';
 // import { getCustomRepository } from 'typeorm';
-import { parseISO } from 'date-fns';
-import { container } from 'tsyringe';
 
-import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
-
+import AppointmentsController from '../controllers/AppointmentsController';
 import ensureAuth from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
+const appointmentsController = new AppointmentsController();
 
 appointmentsRouter.use(ensureAuth);
 
-appointmentsRouter.get('/', async (request, response) => {
-  // const appointmentsRepository = getCustomRepository(AppointmentsRepository);
-
-  // const appointments = await appointmentsRepository.find();
-
-  // return response.json(appointments);
-});
+appointmentsRouter.get('/', appointmentsController.create);
 
 appointmentsRouter.post('/', async (request, response) => {
-  const { provider_id, date } = request.body;
 
-  const parsedDate = parseISO(date);
-
-  const createAppointment = container.resolve(CreateAppointmentService);
-
-  const appointment = await createAppointment.execute({
-    date: parsedDate,
-    provider_id,
-  });
-  return response.json(appointment);
 });
 
 export default appointmentsRouter;
