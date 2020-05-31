a44dd7cc3b619f14774ec20ed6134e9959d9daef
Leandro Arns <leandro.arns@gmail.com>
2020-05-31 13:04:04 -0300

    criando primeiro teste unitario

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,27 @@
-test('sum two numberss', () => {
-  expect(1 + 2).toBe(3);
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(fakeAppointmentsRepository);
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123123123',
+    });
+
+
+    expect(appointment).toHaveProperty('id');
+  });
+
+  // it('should be able to create a new appointment', () => {
+  //   expect().toBe();
+  // });
+
+  // it('should be able to create a new appointment', () => {
+  //   expect().toBe();
+  // });
+
 });
