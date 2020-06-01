fd0bc0733be72865b257777af38ef0a7bb179b38
Leandro Arns <leandro.arns@gmail.com>
2020-06-01 07:31:00 -0300

    finalizando testes unitarios de criaçao de usuarios. Criado limitaçao de criaçao com emails repetidos

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -19,4 +19,27 @@ describe('CreateUser', () => {
     expect(user).toHaveProperty('id');
   });
 
+
+  it('should not be able to create user with a repeated email', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUserService = new CreateUserService(fakeUsersRepository);
+
+    const user = await createUserService.execute({
+      name: 'joão ninguem',
+      email: 'joao@ninguem.com',
+      password: '123456',
+    });
+
+    expect(
+      createUserService.execute({
+        name: 'joão ninguem 2',
+        email: 'joao@ninguem.com',
+        password: '123456',
+      })
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+
+
+
 });
