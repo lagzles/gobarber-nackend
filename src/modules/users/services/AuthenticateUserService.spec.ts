13ead50f3cfeca2773a4ec5b3ac7cc0474fb5751
Leandro Arns <leandro.arns@gmail.com>
2020-06-02 06:56:14 -0300

    finalizando testes unitarios de atutenticaçao

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -28,5 +28,42 @@ describe('CreateUser', () => {
     expect(response.user).toEqual(user);
   });
 
+  it('should not be able authenticate with a non existent user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+    const authenticateUserService = new AuthenticateUserService(fakeUsersRepository, fakeHashProvider);
+
+    expect(
+      authenticateUserService.execute({
+        email: 'jao@ninguem.com',
+        password: '123456',
+      })
+    ).rejects.toBeInstanceOf(AppError);
+
+  });
+
+
+  it('should not be able authenticate a non valid password user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+    const authenticateUserService = new AuthenticateUserService(fakeUsersRepository, fakeHashProvider);
+    const createUserService = new CreateUserService(fakeUsersRepository, fakeHashProvider);
+
+    const user = await createUserService.execute({
+      name: 'joão ninguem',
+      email: 'joao@ninguem.com',
+      password: '123456',
+    });
+
+    expect(
+      authenticateUserService.execute({
+        email: 'jao@ninguem.com',
+        password: 'senha-errada',
+      })
+    ).rejects.toBeInstanceOf(AppError);
+
+  });
+
+
 
 });
