3a63b26e00dff975c63b7f302494c30967fd0fa9
Leandro Arns <leandro.arns@gmail.com>
2020-06-02 07:15:38 -0300

    separando responsabilidades - criando provider de upload de imagens, por enquanto em pasta local

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,13 +2,14 @@ import path from 'path';
 import crypto from 'crypto';
 import multer from 'multer';
 
-const tmpPath = path.resolve(__dirname, '..', '..', 'tmp');
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
 export default {
-  directory: tmpPath,
+  tmpFolder,
+  uploadsFolder: path.resolve(tmpFolder, 'uploads'),
 
   storage: multer.diskStorage({
-    destination: tmpPath,
+    destination: tmpFolder,
     filename(request, file, callback) {
       const fileHash = crypto.randomBytes(10).toString('HEX');
       const fileName = `${fileHash}-${file.originalname}`;
